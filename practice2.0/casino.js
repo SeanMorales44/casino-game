d55b6556857f66eb4e9b21ebbf671818d7973b51
seanmorales <seanmorales44@gamil.com>
2022-01-11 21:04:18 -0600

    Casino Project

---
+++ practice2.0/casino.js
@@ -0,0 +1,19 @@
+const welcome = "Welcome to Vegas Lets \nFlip Coins"
+const charcharacters = {
+dealer:"Alex",
+},charcharacters1 = {
+  player:"Sean"
+}
+
+console.log(welcome)
+console.log(charcharacters)
+
+
+
+const num = Math.random();
+
+if (num < 0.5) {
+    console.log("Heads")
+} else {
+    console.log("Tails");
+}
